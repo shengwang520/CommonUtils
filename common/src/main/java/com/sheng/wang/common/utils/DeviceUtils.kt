8660c02ed247f5629d3050a92de27d4f18ecc0e9
wangshengqiang <527538586@qq.com>
2023-08-02 09:57:50 +0800

    [update]优化设备信息获取

--- common/src/main/java/com/sheng/wang/common/utils/DeviceUtils.kt
+++ common/src/main/java/com/sheng/wang/common/utils/DeviceUtils.kt
@@ -16,22 +16,22 @@ object DeviceUtils {
     private const val DATA_SYSTEM = "data_device_system"
     private const val DATA_UUID = "data_device_uuid"
 
-    private fun sharedPreferences(context: Context): SharedPreferences {
-        return context.getSharedPreferences(DATA_SYSTEM, Context.MODE_PRIVATE)
+    private fun sharedPreferences(context: Context?): SharedPreferences? {
+        return context?.getSharedPreferences(DATA_SYSTEM, Context.MODE_PRIVATE)
     }
 
     /**
      * 保存数据
      */
-    private fun saveData(context: Context, value: String?) {
-        sharedPreferences(context).edit().putString(DATA_UUID, value).apply()
+    private fun saveData(context: Context?, value: String?) {
+        sharedPreferences(context)?.edit()?.putString(DATA_UUID, value)?.apply()
     }
 
     /**
      * 获取数据
      */
-    private fun getData(context: Context): String? {
-        return sharedPreferences(context).getString(DATA_UUID, "")
+    private fun getData(context: Context?): String? {
+        return sharedPreferences(context)?.getString(DATA_UUID, "")
     }
 
 
@@ -52,10 +52,10 @@ object DeviceUtils {
      * 获取设备唯一标识,优先使用androidId,再使用uuid
      */
     @SuppressLint("HardwareIds")
-    fun androidId(context: Context): String? {
+    fun androidId(context: Context?): String? {
         var uuid = getData(context)
         if (TextUtils.isEmpty(uuid)) {
-            val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+            val androidId = Settings.Secure.getString(context?.contentResolver, Settings.Secure.ANDROID_ID)
             uuid = if (TextUtils.isEmpty(androidId) || TextUtils.equals(androidId, "9774d56d682e549c")) { //部分手机可能生成同一androidId
                 UUID.randomUUID().toString()
             } else {
